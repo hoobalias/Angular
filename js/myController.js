4647ba77bb2b64f5bad21671658c18fd64d89309
hoobalias <dirkschulz@hoobalias.com>
2016-09-08 09:49:45 -0700

    Added pokemon images and fixed typos in some pokemon names

--- js/myController.js
+++ js/myController.js
@@ -11,13 +11,13 @@ module.controller('myController', function($scope, $interval, $http) {
     $scope.foods = ["Potatoes", "Rice", "Sweet Potatoes", "Bread", "Beans"];
 
     $scope.pokemon = [{type:'fire', name:'Charmander'},
-                      {type:'fire', name:'Ninetails'},
+                      {type:'fire', name:'Ninetales'},
                       {type:'fire', name:'Magmar'},
                       {type: 'water', name:'Squirtle'},
                       {type: 'water', name:'Gyarados'},
                       {type: 'water', name:'Dewgong'},
                       {type: 'grass', name:'Bulbasaur'},
-                      {type: 'grass', name:'Victreebell'},
+                      {type: 'grass', name:'Victreebel'},
                       {type: 'grass', name:'Exeggutor' }];
 
     $scope.fireSwitch = true;
