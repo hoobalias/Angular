790591708c4e9c8e43889cc05099ad5a15ef299f
hoobalias <dirkschulz@hoobalias.com>
2016-09-26 20:36:32 -0700

    In the middle of getting routing to work

--- js/myApp.js
+++ js/myApp.js
@@ -1,1 +1,17 @@
-var module = angular.module('myApp', ['ngAnimate']);
+var module = angular.module('myApp', ["ngRoute"]);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when("/instinct", {
+            templateUrl: "../templates/instinct.html"
+        })
+        .when("/mystic", {
+            templateUrl: "../templates/mystic.html"
+        })
+        .when("/valor", {
+            templateUrl: "../templates/valor.html"
+        })
+        .otherwise({
+            template: "<h1>None</h1><p>Nothing has been selected</p>"
+        });
+});
