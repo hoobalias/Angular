15b7d5c69f59a5c704a331eaaba95af12863f70b
hoobalias <dirkschulz@hoobalias.com>
2016-10-02 18:08:24 -0700

    Fixed routing templates links, and added background color to each PoGo team toggle link

--- js/myApp.js
+++ js/myApp.js
@@ -3,13 +3,13 @@ var module = angular.module("myApp", ["ngRoute"]);
 module.config(function ($routeProvider) {
     $routeProvider
         .when("/instinct", {
-            templateUrl: "../templates/instinct.html"
+            templateUrl: "templates/instinct.html"
         })
         .when("/mystic", {
-            templateUrl: "../templates/mystic.html"
+            templateUrl: "templates/mystic.html"
         })
         .when("/valor", {
-            templateUrl: "../templates/valor.html"
+            templateUrl: "templates/valor.html"
         })
         .otherwise({
             template: "<h1>None</h1><p>Nothing has been selected</p>"
